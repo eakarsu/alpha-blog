15717f4d7ecd96097f6a3e73721a224ab23f3e17
Erol Akarsu <eakarsu@gmail.com>
2016-02-02 12:41:33 -0500

    pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def show
+    @user = User.find(params[:id])
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
+  end
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -26,6 +31,10 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   private
