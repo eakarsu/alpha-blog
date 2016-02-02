25b9084a09c7ed84b87d1cf7a0e9ff2e7731f97c
Erol Akarsu <eakarsu@gmail.com>
2016-02-02 15:40:42 -0500

    delete extra db

--- db/migrate/20160129170954_add_name_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddNameToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :name, :string
-    ren
-  end
-end
--- db/migrate/20160201165823_add_fields_at_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddFieldsAtToUsers < ActiveRecord::Migration
-  def change
-    rename_column :users,:name,:username
-    add_column :users, :email, :string
-    add_column :users,:timestamps,:datetime
-  end
-end
