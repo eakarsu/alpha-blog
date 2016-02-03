0872bc935d476ded99f4eb0c3c67247e76071081
Erol Akarsu <eakarsu@gmail.com>
2016-02-03 08:50:47 -0500

    rr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
-
+  
     def require_same_user
      if current_user != @user
        flash[:danger] = "You can only edit your own account"
