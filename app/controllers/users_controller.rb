7dbc391ed043b8916a9b2ee39f822bc17070a32a
Erol Akarsu <eakarsu@gmail.com>
2016-02-03 08:57:16 -0500

    signup goes to user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -45,7 +46,7 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
-  
+
     def require_same_user
      if current_user != @user
        flash[:danger] = "You can only edit your own account"
