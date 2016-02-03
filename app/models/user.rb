de5e2d6061f26d4e452acd36bb96074661fe7a21
Erol Akarsu <eakarsu@gmail.com>
2016-02-03 09:24:23 -0500

    admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
 
-  has_many :articles
+  has_many :articles, dependent: :destroy
   before_save { self.email = email.downcase }
   has_secure_password
 end
